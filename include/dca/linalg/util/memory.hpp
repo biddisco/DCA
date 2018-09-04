dfe97806ddf93e10669952fa3107764c8fd9acb5
John Biddiscombe <biddisco@cscs.ch>
2018-09-04 16:28:32 +0200

    Quiet a warning about zeroing a non trivial type

--- include/dca/linalg/util/memory.hpp
+++ include/dca/linalg/util/memory.hpp
@@ -68,13 +68,12 @@ struct Memory<CPU> {
   template <typename ScalarType>
   static std::enable_if_t<std::is_arithmetic<ScalarType>::value == true, void> setToZero(
       ScalarType* ptr, size_t size) {
-    std::memset(ptr, 0, size * sizeof(ScalarType));
+    std::memset(reinterpret_cast<void*>(ptr), 0, size * sizeof(ScalarType));
   }
   template <typename ScalarType>
   static std::enable_if_t<std::is_arithmetic<ScalarType>::value == true, void> setToZero(
-      std::complex<ScalarType>* ptr, size_t /*size*/) {
-      *ptr = {ScalarType(),ScalarType()};
-//    std::memset(ptr, 0, size * sizeof(std::complex<ScalarType>));
+      std::complex<ScalarType>* ptr, size_t size) {
+    std::memset(reinterpret_cast<void*>(ptr), 0, size * sizeof(std::complex<ScalarType>));
   }
 };
 
